a49e224c7c7fbfbf48810003e21a58a746e348f6
Shiva <shanshiva@outlook.com>
2023-12-26 13:29:07 +0530

    Added group routing

---
+++ my-app/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+export default function SignIn() {
+  return (
+    <main>
+      <h1 className="h1-bold">SignIn</h1>
+    </main>
+  );
+}
---
+++ my-app/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <main>
+      <h1 className="h1-bold">SignUp</h1>
+    </main>
+  );
+}
