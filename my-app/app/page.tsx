3c828044da2a113767c75d3639d5334753f95c39
Shiva <shanshiva@outlook.com>
2024-02-01 13:56:45 +0530

    Adding Auth

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,9 @@
-// import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="h1-bold">Hello World! </h1>
-      <h2 className="h2-bold">Shiva is coming!!</h2>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
